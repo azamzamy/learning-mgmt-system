a22963d5f31ca7638089704251dc5e949290e1cb
Zam <abdelrahman.elzamzamy@leasingmarkt.de>
2025-10-13 00:43:04 +0300

    feat: logic for checkign student access is within enrolment dates
    Green phase

--- src/AccessController.php
+++ src/AccessController.php
@@ -9,10 +9,15 @@ class AccessController
     public function canAccess(Student $student, Content $content, DateTime $dateTime): bool
     {
         $course = $content->getCourse();
-        
-        // Rule: Course must have started
-        if ($dateTime < $course->getStartDate()) {
+
+        // Rule: Access must be within the course's active period
+        if ($dateTime < $course->getStartDate() || $dateTime > $course->getEndDate()) {
             return false;
+        }
+
+        $enrolment = $student->getEnrolmentForCourse($course);
+        if ($enrolment === null || $dateTime < $enrolment->getStartDate() || $dateTime > $enrolment->getEndDate()) {
+            return false; // No enrolment found
         }
 
         return true;
